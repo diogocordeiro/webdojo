93d48185551ff0b47c9ac1b31e1091080e95e09c
Diogo Santos <diogo.ufrpe@gmail.com>
2025-06-02 19:34:07 +0100

    consultancy tests: Add test for consultancy page

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 describe('Consultancy Form', () => {
-  it('should submit the individual consultancy form', () => {
+  it.only('should submit the individual consultancy form', () => {
     cy.start()
 
     cy.submitLoginForm('papito@webdojo.com', 'katana123')
@@ -96,13 +96,14 @@ describe('Consultancy Form', () => {
 
     cy.contains('button', 'Enviar formulário').click()
 
-    cy.get('.modal')
+    cy.get('.modal', { timeout: 7000 })
       .should('be.visible')
       .find('.modal-content')
       .should('be.visible')
-      .and('have.text', 'Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
-
-    
+      .and(
+        'have.text',
+        'Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.'
+      )
   })
 
   it('Verify required fields', () => {
